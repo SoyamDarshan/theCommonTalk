edcfbba30971c94c44c669b39bf721e96f516736
SoyamDarshan <soyam.darshan@gmail.com>
2019-04-03 02:44:19 +0530

    Updated Settings

--- theCommonTalk/settings.py
+++ theCommonTalk/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@xgw6+$(y8ztp&e5o_0ocsiuh=1ll(cw)+507km^zdjp2=o2lr'
-
+#SECRET_KEY = '@xgw6+$(y8ztp&e5o_0ocsiuh=1ll(cw)+507km^zdjp2=o2lr'
+SECRET KEY = os.environ.get('SECRET_DJANGO_KEY_PROD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thecommontalk.herokuapp.com']
 
 
 # Application definition
