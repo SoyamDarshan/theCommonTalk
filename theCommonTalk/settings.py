e09ad3c1eded8f94a0d0be081989d30bbf270c56
SoyamDarshan <soyam.darshan@gmail.com>
2019-04-03 02:45:14 +0530

    Updated Settings

--- theCommonTalk/settings.py
+++ theCommonTalk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '@xgw6+$(y8ztp&e5o_0ocsiuh=1ll(cw)+507km^zdjp2=o2lr'
-SECRET KEY = os.environ.get('SECRET_DJANGO_KEY_PROD')
+SECRET_KEY = os.environ.get('SECRET_DJANGO_KEY_PROD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
